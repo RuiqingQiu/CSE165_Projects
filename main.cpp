8d82f520f53777b92f192978ca0def449e7e1581
Mingshan Wang <margartewm3@gmail.com>
2015-01-08 13:22:05 -0800

    add some comments

--- main.cpp
+++ main.cpp
@@ -19,24 +19,25 @@ int main (void)
     btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
     btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
     
+    //the actual solver
     btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
     
+    //the world
     btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
     
     dynamicsWorld->setGravity(btVector3(0, -10, 0));
-    
-    
+
     btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
     
     btCollisionShape* fallShape = new btSphereShape(1);
     
     
     btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
+    
     btRigidBody::btRigidBodyConstructionInfo
     groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
     btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
     dynamicsWorld->addRigidBody(groundRigidBody);
-    
     
     btDefaultMotionState* fallMotionState =
     new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
